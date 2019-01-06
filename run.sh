49ff1d5dcf93490fa613e733a95837dcb73dee48
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2019-01-06 15:41:51 +0100

    UDP port 514

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ tag="$1"
 docker run --rm -it \
            -v "${HOME}/.config/docker/etc_syslog-ng_syslog-ng.conf":/etc/syslog-ng/syslog-ng.conf \
            -v /srv/array1/netsyslog:/srv/syslognet \
-           -p 514:514 -p 601:601 -p 6514:6514 \
+           -p 514:514/udp -p 601:601 -p 6514:6514 \
            "mausy5043/syslog-ng-server:${tag}"
