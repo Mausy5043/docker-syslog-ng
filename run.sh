ba50833b98ab770483e57e881cf1593a756b7580
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2019-01-06 15:18:19 +0100

    @s are illegal

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 tag="$1"
 
 docker run --rm -it \
-           -v ../config/etc@syslog-ng@syslog-ng.conf:/etc/syslog-ng/syslog-ng.conf \
+           -v ../config/etc_syslog-ng_syslog-ng.conf:/etc/syslog-ng/syslog-ng.conf \
            -v /srv/array1/netsyslog:/srv/syslognet \
            "mausy5043/syslog-ng-server:${tag}"
