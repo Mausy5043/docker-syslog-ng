5d86e8f4fc930f188c4c806ef14976378d1b8f6b
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2019-01-06 15:34:29 +0100

    make path more absolute

--- run.sh
+++ run.sh
@@ -2,8 +2,7 @@
 
 tag="$1"
 
-cd ..
 docker run --rm -it \
-           -v "${PWD}/config/etc_syslog-ng_syslog-ng.conf":/etc/syslog-ng/syslog-ng.conf \
+           -v "${HOME}/.config/docker/etc_syslog-ng_syslog-ng.conf":/etc/syslog-ng/syslog-ng.conf \
            -v /srv/array1/netsyslog:/srv/syslognet \
            "mausy5043/syslog-ng-server:${tag}"
