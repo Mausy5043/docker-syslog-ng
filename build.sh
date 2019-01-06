92e62a0c84e61269ef66442685af841ded93e013
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2019-01-06 15:14:27 +0100

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+tag=$1
+
 # build a local image
-docker build -t mausy5043/syslog-ng-server:latest .
+docker build -t "mausy5043/syslog-ng-server:${tag}" .
--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-options="$@"
+tag="$1"
 
-docker run --rm -it mausy5043/syslog-ng-server:latest "${options}"
+docker run --rm -it \
+           -v ../config/etc@syslog-ng@syslog-ng.conf:/etc/syslog-ng/syslog-ng.conf \
+           -v /srv/array1/netsyslog:/srv/syslognet \
+           "mausy5043/syslog-ng-server:${tag}"
