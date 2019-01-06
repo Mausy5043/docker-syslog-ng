b3c59b2d901e6883b7f096c55a5477ae1cc52ed8
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2019-01-06 15:45:10 +0100

    daemonize with name

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@
 
 tag="$1"
 
-docker run --rm -it \
+docker run --rm -d \
            -v "${HOME}/.config/docker/etc_syslog-ng_syslog-ng.conf":/etc/syslog-ng/syslog-ng.conf \
            -v /srv/array1/netsyslog:/srv/syslognet \
            -p 514:514/udp -p 601:601 -p 6514:6514 \
+           --name syslog-ng
            "mausy5043/syslog-ng-server:${tag}"
