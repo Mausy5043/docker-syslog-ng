abb3af903a21301fca4124a0169dfb11ce33e9c3
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2019-01-06 15:23:00 +0100

    must be absolute path

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 tag="$1"
 
+cd ..
 docker run --rm -it \
-           -v ../config/etc_syslog-ng_syslog-ng.conf:/etc/syslog-ng/syslog-ng.conf \
+           -v "${PWD}/config/etc_syslog-ng_syslog-ng.conf":/etc/syslog-ng/syslog-ng.conf \
            -v /srv/array1/netsyslog:/srv/syslognet \
            "mausy5043/syslog-ng-server:${tag}"
