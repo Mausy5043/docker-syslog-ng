05208b9f54d63517a87d0186d5aef4f68d230b78
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2019-01-08 16:49:25 +0100

    fix

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 tag="$1"
 
 docker run --rm \
-			-d \
-			--restart unless-stopped \
+           -d \
+           --restart unless-stopped \
            -v "${HOME}/.config/docker/etc_syslog-ng_syslog-ng.conf":/etc/syslog-ng/syslog-ng.conf \
            -v /srv/array1/netsyslog:/srv/syslognet \
            -p 514:514/udp -p 601:601 -p 6514:6514 \
            --name syslog-ng \
-           -h syslog-ng
+           -h syslog-ng \
            "mausy5043/syslog-ng-server:${tag}"
