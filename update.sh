33be179474f3bdfff2e711e56db04da82ee8740b
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2019-01-12 09:48:24 +0100

    allow for rebuilds

--- update.sh
+++ update.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+cmd="{$1}"
+
+# Find out where we're running from
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 git fetch origin master
 git reset --hard FETCH_HEAD
 git clean -df
 git pull
+
+if [ "${cmd}" == "--all" ]; then
+  "${SCRIPT_DIR}"/build.sh local || exit 1
+  "${SCRIPT_DIR}"/stop.sh || exit 1
+  "${SCRIPT_DIR}"/run.sh local || exit 1
+fi
