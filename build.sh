4618c4678d15ea23b3c4f2029bce443c94198fa6
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2019-01-13 12:06:07 +0100

    always build inside repo directory

--- build.sh
+++ build.sh
@@ -2,5 +2,15 @@
 
 tag=$1
 
+# Find out where we're running from
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ ! -f Dockerfile ]; then
+  echo "Not allowed. First execute:"
+  echo "cd ${SCRIPT_DIR}"
+  echo " Then try again."
+  exit 1
+fi
+
 # build a local image
 docker build -t "mausy5043/syslog-ng-server:${tag}" .
