e10dd492c87166fed92c0499e7a924b4b13ad306
Maurice (mausy5043) Hendrix <mausy5043@gmail.com>
2019-01-13 12:30:16 +0100

    config file now in payload

--- run.sh
+++ run.sh
@@ -2,9 +2,19 @@
 
 tag="$1"
 
+# Find out where we're running from
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ ! -f Dockerfile ]; then
+  echo "Not allowed. First execute:"
+  echo "cd ${SCRIPT_DIR}"
+  echo " Then try again."
+  exit 1
+fi
+
 docker run -d \
            --restart unless-stopped \
-           -v "${HOME}/.config/docker/etc_syslog-ng_syslog-ng.conf":/etc/syslog-ng/syslog-ng.conf \
+           -v "${PWD}/config/etc_syslog-ng_syslog-ng.conf":/etc/syslog-ng/syslog-ng.conf \
            -v /srv/array1/netsyslog:/srv/syslognet \
            -p 514:514/udp -p 601:601 -p 6514:6514 \
            --name syslog-ng \
